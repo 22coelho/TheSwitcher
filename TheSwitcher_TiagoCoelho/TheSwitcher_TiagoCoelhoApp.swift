f66bc17e843fa34f3df86a2a9e029a7897d5bbe1
coelhot-ppb <tiago.coelho@paddypowerbetfair.com>
2023-05-08 21:52:30 +0100

    Initial Commit

---
+++ TheSwitcher_TiagoCoelho/TheSwitcher_TiagoCoelhoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSwitcher_TiagoCoelhoApp.swift
+//  TheSwitcher_TiagoCoelho
+//
+//  Created by Tiago Coelho on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheSwitcher_TiagoCoelhoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
